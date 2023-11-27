07143e2799a9aa9d671a25f31f91789de5aca3bf
pevescarol <carol.peves@gmail.com>
2023-11-27 18:45:01 -0300

    se agrega CartProvider y componente Cart

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,16 @@
+import Cart from '@/components/Cart'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Products from '@/components/Products'
+import { CartProvider } from '@/context/cart'
 
 export default function Home() {
   
   return (
-    <>
+    <CartProvider>
       <Header />
+      <Cart />
       <Products />
-    </>
+    </CartProvider>
   )
 }
