ad7ee9f86cf8c6a7e0e4f8bb1e92b70b0e55cc45
pevescarol <carol.peves@gmail.com>
2023-11-27 18:47:20 -0300

    se modifica el tipo de dato de precio

--- utils/api.js
+++ utils/api.js
@@ -10,6 +10,14 @@ export const fetchProducts = async () => {
     const result = Papa.parse(response.data, {
       header: true,
       transformHeader: header => header.toLowerCase().replace(/ /g, '_'), // convertimos encabezados a minúsculas y reemplazar espacios con guiones bajos
+      transform: (value, header) => {
+        if (header === 'price') {
+          // Eliminar caracteres que no sean números, como símbolos de moneda o separadores
+          const numericValue = parseFloat(value.replace(/[^0-9.]/g, ''));
+          return !isNaN(numericValue) ? numericValue : value; // Si es un número válido, devolverlo; de lo contrario, mantener el valor original
+        }
+        return value;
+      },
     });
 
     return result.data;
