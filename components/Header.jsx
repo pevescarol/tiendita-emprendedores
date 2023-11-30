3f37f3b9d31a32e74c106bdb9ab58d3e1983c40b
pevescarol <carol.peves@gmail.com>
2023-11-30 18:16:31 -0300

    update tailwindcss

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,19 @@
+import Link from "next/link"
+import Cart from "./Cart"
+import Image from "next/image"
+import imgLogo from '../public/mitienda.jpg'
 
 const Header = () => {
   return (
-    <header className='mx-auto w-full h-48 flex items-center justify-center flex-col gap-8'>
-      <h1>Almacen Shop</h1>
+    <header className='w-full fixed top-0 bg-white'>
+      <div className="mx-auto max-w-4xl w-full h-20 md:h-28 flex items-center justify-between px-6">
+        <div>
+          <Link href='/'>
+            <Image src={imgLogo} width={200} height={100} alt="logo dalone" className="w-full" />
+          </Link>
+        </div>
+        <Cart />
+      </div>
     </header>
   )
 }
