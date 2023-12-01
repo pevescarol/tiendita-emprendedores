dbe90413081d13d2fc5654d944e54f604d2ebc53
pevescarol <carol.peves@gmail.com>
2023-12-01 00:14:02 -0300

    se agrea logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,10 +6,10 @@ import imgLogo from '../public/mitienda.jpg'
 const Header = () => {
   return (
     <header className='w-full fixed top-0 bg-white'>
-      <div className="mx-auto max-w-4xl w-full h-20 md:h-28 flex items-center justify-between px-6">
+      <div className="mx-auto max-w-4xl w-full h-20 md:h-28 flex items-center justify-between pl-2 pr-4 md:p-0">
         <div>
           <Link href='/'>
-            <Image src={imgLogo} width={200} height={100} alt="logo dalone" className="w-full" />
+            <Image src={imgLogo} width={200} height={100} alt="logo dalone" className="w-[130px] md:w-full h-20" />
           </Link>
         </div>
         <Cart />
