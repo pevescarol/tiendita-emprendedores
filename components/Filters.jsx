96779d6a0bd24c4d1a2e5c1064096011bbcad1ec
pevescarol <carol.peves@gmail.com>
2023-12-02 15:22:21 -0300

    cambios en los estilos css

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -9,11 +9,11 @@ const Filters = ({ filters, setFilters }) => {
 
 
   return (
-    <section className='flex w-full items-center justify-evenly text-sm font-bold mt-36'>
-      <div className='flex gap-4'>
-        <label htmlFor="category">Categoria</label>
-        <select id="category" onChange={handleCategoryChange} value={filters.category}>
-          <option value="all">Todas</option>
+    <section className='flex w-full items-center justify-center'>
+      <div className='flex flex-col mx-auto items-center gap-6 w-[200px]'>
+        <label htmlFor="category" className='text-lg font-semibold mb-3'>Elige la categoría</label>
+        <select id="category" onChange={handleCategoryChange} value={filters.category} className='bg-[#e8f7eb] text-emerald-600 font-semibold p-3 rounded-xl w-full text-center text-lg border-none'>
+          <option value="all">Todos</option>
           <option value="tortas">Tortas</option>
           <option value="budines">Budines</option>
           <option value="desayunos">Desayunos</option>
--- components/Header.jsx
+++ components/Header.jsx
@@ -5,11 +5,11 @@ import imgLogo from '../public/mitienda.jpg'
 
 const Header = () => {
   return (
-    <header className='w-full fixed top-0 bg-white'>
-      <div className="mx-auto max-w-4xl w-full h-20 md:h-28 flex items-center justify-between pl-2 pr-4 md:p-0">
+    <header className='w-full fixed top-0 bg-white z-[9999]'>
+      <div className="mx-auto max-w-5xl w-full h-20 md:h-28 flex items-center justify-between pl-2 pr-4 md:p-0">
         <div>
           <Link href='/'>
-            <Image src={imgLogo} width={200} height={100} alt="logo dalone" className="w-[130px] md:w-full h-20" />
+            <Image src={imgLogo} width={300} height={300} alt="logo dalone" className="w-[140px] md:w-full h-20" />
           </Link>
         </div>
         <Cart />
