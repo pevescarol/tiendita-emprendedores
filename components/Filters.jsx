95f577fc917d4050095ec0aa55ad317fa5809a27
pevescarol <carol.peves@gmail.com>
2024-01-06 14:16:05 -0300

    se cambia valores y modifica los estilos

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -9,13 +9,13 @@ const Filters = ({ filters, setFilters }) => {
 
 
   return (
-    <section className='flex w-full items-center justify-center'>
-      <div className='flex flex-col mx-auto items-center gap-6 w-[200px]'>
-        <label htmlFor="category" className='text-lg font-semibold mb-3'>Elige la categoría</label>
-        <select id="category" onChange={handleCategoryChange} value={filters.category} className='bg-[#e8f7eb] text-emerald-600 font-semibold p-3 rounded-xl w-full text-center text-lg border-none'>
+    <section className='flex w-full items-center my-6'>
+      <div className='flex flex-col md:flex-row justify-between items-center gap-6 w-full'>
+        <label htmlFor="category" className='text-2xl text-main font-semibold mb-3'>Productos</label>
+        <select id="category" onChange={handleCategoryChange} value={filters.category} className='bg-grayback text-main cursor-pointer font-semibold p-3 rounded-xl w-[200px] text-center text-lg border-none'>
           <option value="all">Todos</option>
           <option value="tortas">Tortas</option>
-          <option value="budines">Budines</option>
+          <option value="especiales">Especiales</option>
           <option value="desayunos">Desayunos</option>
         </select>
       </div>
