34499c18be53846c770150038bb5b8474c8661af
pevescarol <carol.peves@gmail.com>
2024-01-06 14:19:35 -0300

    se agrega nuevo componente y se modifican los estilos

--- components/Products.jsx
+++ components/Products.jsx
@@ -7,6 +7,7 @@ import Filters from './Filters';
 import { useCart } from '@/hooks/useCart';
 import { parseCurrency } from '@/utils/currency';
 import { BsCart3, BsTrash } from 'react-icons/bs'
+import ProductModal from './ProductModal';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -15,6 +16,8 @@ const Products = () => {
   });
   const { addToCart, removeFromCart, cart } = useCart()
   const [loading, setLoading] = useState(true)
+
+  const [selectedProduct, setSelectedProduct] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -45,13 +48,22 @@ const Products = () => {
 
   const filteredProducts = filterProducts(products);
 
+  // Modal
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedProduct(null);
+  };
+
 
   return (
-    <main className='flex flex-col items-center justify-center max-w-5xl mx-auto '>
+    <main className='flex flex-col items-center justify-center max-w-5xl mx-auto px-6 '>
       {/* Filtrar productos */}
       <Filters filters={filters} setFilters={setFilters} />
       {/* Productos */}
-      <ul className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-5 mt-16 px-8 items-center">
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-5 mt-8 md:px-0 items-center mx-auto">
         {loading ? (
           // si el loading es true
           <div className="col-span-2 flex flex-col gap-4 items-center justify-center">
@@ -65,24 +77,30 @@ const Products = () => {
             const isProductInCart = checkProductInCart(product)
     
             return (
-              <li key={product.id} className="flex h-[450px] w-[320px] flex-col overflow-hidden bg-[#e8f7eb] rounded-2xl drop-shadow-md md:h-[250px] md:w-full md:flex-row">
+              <li 
+                key={product.id} 
+                className="flex h-[450px] w-[320px] flex-col overflow-hidden bg-white rounded-2xl drop-shadow md:h-[200px] md:w-full md:flex-row"
+              >
                 {/* item del producto */}
-                <div className='h-[240px] md:h-full md:w-[250px] bg-slate-400'>
-                  <Image src={product.image} alt={product.title} width={300} height={300} className="object-cover h-full w-full " />
+                <div className='h-[240px] md:h-full md:w-[250px] bg-grayback'>
+                  <Image src={product.image} alt={product.title} loading='lazy' width={300} height={300} className="object-cover h-full w-full " />
                 </div>
-                <div className='flex h-[350px] flex-col justify-center space-y-4 px-6 md:h-full md:w-full '>
-                  <h2 className="text-black text-xl font-bold">{product.title}</h2>
-                  <p className='text-slate-600 text-sm'>probando texto bla bla rfdfnj dsjdnfjdfsdnjndj djsndjnsndjsdn bla bla rfdfnj dsjdnfjdfsdnjndj djsndjnsndjsdn</p>
-                  <div className='flex items-center justify-between'>
-                    <span className="text-black text-lg font-semibold">{parseCurrency(product.price)}</span>
+                <div className='text-main flex h-[350px] flex-col justify-center space-y-5 px-4 md:h-full md:w-full '>
+                  <h2 className="text-xl font-bold">{product.title}</h2>
+                  <div>
+                    <p className=' text-sm overflow-hidden whitespace-nowrap overflow-ellipsis w-[220px]'>{product.description}</p>
+                    <span onClick={() => handleProductClick(product)} className='cursor-pointer text-xs font-semibold'> Ver más</span>
+                  </div>
+                  <div className='flex items-center justify-between space-x-3'>
+                    <span className="text-lg font-semibold">{parseCurrency(product.price)}</span>
                     <button 
                       className={`rounded-xl px-4 py-3  flex items-center justify-center font-bold transition-all duration-200 ease-in-out 
-                        ${isProductInCart ? 'bg-red-500 text-white hover:bg-white hover:text-red-500' : 'bg-emerald-600 text-white hover:bg-white hover:text-emerald-600'}`}
+                        ${isProductInCart ? 'bg-redx text-white hover:bg-white hover:text-redx' : 'bg-greenw text-white hover:bg-white hover:text-greenw'}`}
                       onClick={() => isProductInCart ? removeFromCart(product) : addToCart(product)}
                     >
-                        {
-                          isProductInCart ? <span className='flex items-center gap-2 text-xs'><BsTrash className='text-lg' />Eliminar</span> : <span className='flex items-center gap-2 text-xs'><BsCart3 className='text-lg' />Agregar</span>
-                        }
+                      {
+                        isProductInCart ? <span className='flex items-center gap-2 text-sm'><BsTrash className='text-lg' />Eliminar</span> : <span className='flex items-center gap-2 text-sm'><BsCart3 className='text-lg' />Agregar</span>
+                      }
                     </button>
                   </div>
                 </div>
@@ -91,6 +109,11 @@ const Products = () => {
           }) 
         )}
       </ul>
+
+      {/* Renderizar el modal si un producto está seleccionado */}
+      {selectedProduct && (
+        <ProductModal product={selectedProduct} onClose={handleCloseModal} />
+      )}
 
     </main>
   )
