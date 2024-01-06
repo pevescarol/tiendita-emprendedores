a6b6dfeea9cea23df14f35e87e5385d882512f4e
pevescarol <carol.peves@gmail.com>
2024-01-06 14:11:20 -0300

    config generales

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,13 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      main: '#383634',
+      greenw: '#40AA54',
+      redx: '#e45959',
+      grayback: '#edeae7',
+      white: '#fff',
+      greensect: '#4c7c7d',
     },
   },
   plugins: [],
